222e44aa17354118999a0f29a914453a09754cbd
mshariqq <shariqq.com@gmail.com>
2023-05-30 16:50:34 +0530

    Databse, Models setup
    Genral tables setup users, settings, env data

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,43 +2,109 @@
 
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Constants\Status;
+use App\Traits\Searchable;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
-{
-    use HasApiTokens, HasFactory, Notifiable;
+class User extends Authenticatable {
+    use HasApiTokens, Searchable;
 
     /**
-     * The attributes that are mass assignable.
+     * The attributes that should be hidden for arrays.
      *
-     * @var array<int, string>
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
-
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
+     * @var array
      */
     protected $hidden = [
-        'password',
-        'remember_token',
+        'password', 'remember_token', 'ver_code',
     ];
 
     /**
-     * The attributes that should be cast.
+     * The attributes that should be cast to native types.
      *
-     * @var array<string, string>
+     * @var array
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
+        'address'           => 'object',
+        'kyc_data'          => 'object',
+        'ver_code_send_at'  => 'datetime',
     ];
+
+    protected $data = [
+        'data' => 1,
+    ];
+
+    public function loginLogs() {
+        return $this->hasMany(UserLogin::class);
+    }
+
+    public function transactions() {
+        return $this->hasMany(Transaction::class)->orderBy('id', 'desc');
+    }
+
+    public function deposits() {
+        return $this->hasMany(Deposit::class)->where('status', '!=', Status::PAYMENT_INITIATE);
+    }
+
+    public function withdrawals() {
+        return $this->hasMany(Withdrawal::class)->where('status', '!=', Status::PAYMENT_INITIATE);
+    }
+
+    public function referrer() {
+        return $this->belongsTo(User::class, 'ref_by');
+    }
+
+    public function referrals() {
+        return $this->hasMany(User::class, 'ref_by');
+    }
+
+    public function allReferrals() {
+        return $this->referrals()->with('referrer');
+    }
+
+    public function fullname(): Attribute {
+        return new Attribute(
+            get:fn() => $this->firstname . ' ' . $this->lastname,
+        );
+    }
+
+    // SCOPES
+    public function scopeActive() {
+        return $this->where('status', Status::USER_ACTIVE);
+    }
+
+    public function scopeBanned() {
+        return $this->where('status', Status::USER_BAN);
+    }
+
+    public function scopeEmailUnverified() {
+        return $this->where('ev', Status::NO);
+    }
+
+    public function scopeMobileUnverified() {
+        return $this->where('sv', Status::NO);
+    }
+
+    public function scopeKycUnverified() {
+        return $this->where('kv', Status::KYC_UNVERIFIED);
+    }
+
+    public function scopeKycPending() {
+        return $this->where('kv', Status::KYC_PENDING);
+    }
+
+    public function scopeEmailVerified() {
+        return $this->where('ev', Status::VERIFIED);
+    }
+
+    public function scopeMobileVerified() {
+        return $this->where('sv', Status::VERIFIED);
+    }
+
+    public function scopeWithBalance() {
+        return $this->where('balance', '>', 0);
+    }
+
 }
